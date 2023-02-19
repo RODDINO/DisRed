3264d28add510e708d6ebd77d13539b6efae4b69
RODDINO <88658034+RODDINO@users.noreply.github.com>
2023-02-19 22:30:14 +0100

    Create info.py

---
+++ runpar/info.py
@@ -0,0 +1,16 @@
+{
+  "author": [
+    "Roddo"
+  ],
+  "min_bot_version": "3.4.0",
+  "description": "PArole a caso",
+  "hidden": false,
+  "install_msg": "sium si é installata usa !runpar`",
+  "short": "Dad joke bot",
+  "end_user_data_statement": "This cog does not store any End User Data",
+  "tags": [
+    "roddo",
+    "utils",
+    "tools"
+  ]
+}
