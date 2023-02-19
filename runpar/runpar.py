a5381810f91a7219702e0b126bb02373a8a6eb66
RODDINO <88658034+RODDINO@users.noreply.github.com>
2023-02-19 22:33:35 +0100

    Update runpar.py

--- runpar/runpar.py
+++ runpar/runpar.py
@@ -1,32 +1,25 @@
-import discord
-from discord.ext import commands
 import random
+from redbot.core import commands
 
-class RunParCog(commands.Cog):
-
+class Ranpar(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.word_list = ["parola1", "parola2", "parola3", "parola4", "parola5", "parola6", "parola7", "parola8", "parola9", "parola10"] # lista di parole casuali
 
     @commands.command()
-    async def runpar(self, ctx, num: int):
-        # Apriamo i file di testo contenenti le parole casuali
-        with open("nouns.txt", "r") as f:
-            nouns = [line.strip() for line in f.readlines()]
-        with open("adjectives.txt", "r") as f:
-            adjectives = [line.strip() for line in f.readlines()]
+    async def ranpar(self, ctx, num: int):
+        if num <= 0:
+            await ctx.send("Il numero deve essere maggiore di 0.")
+            return
 
-        # Prendiamo un numero di parole casuali uguale a quello richiesto
-        words = []
-        for i in range(num):
-            noun = random.choice(nouns)
-            adjective = random.choice(adjectives)
-            words.append(f"{adjective} {noun}")
+        if num > len(self.word_list):
+            await ctx.send(f"Sono disponibili solo {len(self.word_list)} parole.")
+            return
 
-        # Costruiamo il messaggio di risposta
-        response = " ".join(words)
+        random_words = random.sample(self.word_list, num) # seleziona un numero di parole casuali dalla lista
+        message = " ".join(random_words) # unisce le parole casuali in una stringa
 
-        # Inviamo il messaggio di risposta
-        await ctx.send(response)
+        await ctx.send(message)
 
 def setup(bot):
-    bot.add_cog(RunParCog(bot))
+    bot.add_cog(Ranpar(bot))
